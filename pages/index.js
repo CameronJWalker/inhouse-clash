6bc466b8ae4b64124f1e11d9e69608248fa756a3
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-10 21:42:48 -0700

    added form container, with input field and submit button. and some styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import React, { useState } from 'react';
+import Form from "../styles/Form.module.css"
+
 export default function Home() {
+
+  const [ roster, setRoster ] = useState([]);
+
+
   return (
-    <main className ="page-container">
-      <h1>5v5 team randomizer</h1>
+    <main className = "page-container">
+      <form className={Form.container}>
+        <input className={Form.summonersName}type="text" name="summoner" placeholder="Enter Summoner's Name..." />
+        <button className={Form.submit}> Submit </button>
+      </form>
     </main>
   )
 }
