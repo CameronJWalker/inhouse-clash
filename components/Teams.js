75f18829813b600ce4fa60aabff537bcf041cf2f
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-15 08:21:57 -0700

    some styling

--- components/Teams.js
+++ components/Teams.js
@@ -8,20 +8,22 @@ export default function Teams(props) {
                 <>
                     <div className={Team.roster}>
                         {props.roster.map((ros, index)=>(
-                            <li key={index}>{ros}</li>
+                            <ol key={index}>{ros}</ol>
                         ))}
                     </div>
                 </>
             ) : (
                 <div className={Team.teams}>
                     <div className={Team.one}>
+                        <h2 className={Team.header}>Team 1</h2>
                         {props.team1.map((team, index) =>(
-                            <li key={index}>{team}</li>
+                            <ol key={index}>{team}</ol>
                         ))}
                     </div>
                     <div className={Team.two}>
+                        <h2 className={Team.header}>Team 2</h2>
                         {props.team2.map((team, index) =>(
-                            <li key={index}>{team}</li>
+                            <ol key={index}>{team}</ol>
                         ))}
                     </div>
                 </div>
